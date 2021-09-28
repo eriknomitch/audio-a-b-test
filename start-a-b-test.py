177cd6a165b20c9654bb64c9aec87b203c54ade3
Erik Nomitch <enomitch@gmail.com>
2021-09-28 00:07:18 -0500

    Add menu and shuffle

--- start-a-b-test.py
+++ start-a-b-test.py
@@ -1,16 +1,30 @@
 import os
 import sys
+from ipdb import set_trace
+import random
 
 from utility import *
 
 # ------------------------------------------------
 # ------------------------------------------------
 # ------------------------------------------------
-# ------------------------------------------------
-# ------------------------------------------------
-# ------------------------------------------------
-audio_file_information("audio_pairs/test_flac.flac")
-audio_file_information("audio_pairs/test_mp3_320.mp3")
-audio_file_information("audio_pairs/test_mp3_128.mp3")
+def play_random_file_from_pairs(test_name):
+    test_path = f"audio_pairs/{test_name}"
+    to_play = random_file_from_dir(test_path)
+    file_choices = files_in_directory(test_path)
 
-# play_audio_file("audio_pairs/test_flac.flac")
+    random.shuffle(file_choices)
+
+    clear_console_screen()
+
+    play_audio_file(to_play)
+    clear_console_screen()
+
+    prompt_menu("Your guess", map(lambda f: audio_file_information(f, True), file_choices))
+    print()
+    print()
+    print(to_play)
+
+    audio_file_information(to_play)
+
+play_random_file_from_pairs("fragments_of_time")
--- utility.py
+++ utility.py
@@ -1,29 +1,34 @@
 import os
+import sys
 import random
 import mutagen
 import ipdb
 import pexpect
 
-def spawn_an_interactive_command(command):
+def clear_console_screen():
+    """
+    Clear the console screen.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def spawn_interactive_command(command, args=[]):
     """
     Spawns an interactive shell command using pexpect.
     """
-    child = pexpect.spawn(command)
+    child = pexpect.spawn(command, args)
     child.interact()
 
-def audio_file_information(path, skip_print=False):
-    audio_file = mutagen.File(path)
-    info_pprint = audio_file.info.pprint()
-    if not skip_print:
-        print(info_pprint)
-    return info_pprint
+def files_in_directory(dir, ext=False, absolute_path=True):
+    """
+    Return list of files in a directory.
+    """
+    if absolute_path:
+        return list(map(lambda f: os.path.abspath(os.path.join(dir, f)), files_in_directory(dir, ext, False)))
 
-def play_audio_file(path):
-    p = pexpect.spawn("vlc", ["--intf", "rc", path])
-    p.interact()
-    print(p)
-    return p
-
+    if ext:
+        return [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and f.endswith(ext)]
+    else:
+        return [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
 
 def random_file_from_dir(dir, ext=False):
     """
@@ -34,5 +39,22 @@ def random_file_from_dir(dir, ext=False):
         files = [f for f in files if f.endswith(ext)]
     return os.path.join(dir, random.choice(files))
 
+def prompt_menu(message, choices):
+    """
+    Show the user their choices (prefixed with A, B, C...) and prompt them for their choice.
+    """
+    print(message)
+    for i, choice in enumerate(choices):
+        print("{}) {}".format(chr(i + 65), choice))
+    return input("Choice: ")
 
+def audio_file_information(path, skip_print=False):
+    audio_file = mutagen.File(path)
+    info_pprint = audio_file.info.pprint()
+    if not skip_print:
+        print(info_pprint)
+    return info_pprint
+
+def play_audio_file(path):
+    spawn_interactive_command("vlc", ["--intf", "rc", path])
 
